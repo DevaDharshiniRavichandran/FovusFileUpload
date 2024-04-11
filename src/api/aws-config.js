a200e50cf5e918151a6700daf2131f71ca720cfa
deva <devaravich@gmail.com>
2024-04-10 23:42:05 -0700

    lambda javascript function updated

--- src/api/aws-config.js
+++
@@ -1,8 +0,0 @@
-// aws-config.js
-import AWS from 'aws-sdk';
-
-AWS.config.update({
-  accessKeyId: 'AKIAU6GDZUUVD63BHSPH',
-  secretAccessKey: '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',
-  region: 'us-east-2',
-});
